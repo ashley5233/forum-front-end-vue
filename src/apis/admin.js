a126d645780d6ae16be2114d3020ba4c64969269
ashley huang <asashu81@hotmail.com>
2021-07-02 18:57:13 +0800

    Add AdminRestaurantTable API

--- src/apis/admin.js
+++ src/apis/admin.js
@@ -14,6 +14,21 @@ export default {
       return apiHelper.post('/admin/restaurants', formData, {
         headers: { Authorization: `Bearer ${getToken()}` }
       })
+    },
+    getRestaurants() {
+      return apiHelper.get('/admin/restaurants', {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+    },
+    showRestaurant({ restaurantId }) {
+      return apiHelper.get(`/admin/restaurants/${restaurantId}`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
+    },
+    deleteRestaurant({ restaurantId }) {
+      return apiHelper.delete(`/admin/restaurants/${restaurantId}`, {
+        headers: { Authorization: `Bearer ${getToken()}` }
+      })
     }
   }
 }
